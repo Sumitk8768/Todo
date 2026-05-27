f16673372648b0f0eee10dd2399e542f31e52958
Sumit <sumitk72004@gmail.com>
2026-05-27 22:05:16 +0530

    POST API craeted successfully

--- self-build/src/app.js
+++ self-build/src/app.js
@@ -1,8 +1,39 @@
 import express from "express"
-
+import noteModel from "./models/notes.model.js"
 let app = express()
 app.use(express.json())
 
-// 
+//* @routes POST /api/notes
+//* @description Create a new note need title and description in the rewuest body
+//* @access Public
+
+app.post("/api/notes", async (req, res)=>{
+    let {title, description} = req.body
+    
+    // -----  Validatation ---------
+    if(!title.trim)
+        return res.status(400).json({error:"Title not found"})
+    
+    if(!description)
+        return res.status(400).json({error:"Description not found"})
+
+    if(title.trim().length < 3) {
+        return res.status(400).json({error: "Title must be at least 3 character long"})
+    }
+
+     if(title.trim().length < 10) {
+        return res.status(400).json({error: "Description must be at least 10 character long"})
+    }
+
+    // IF validation passes then, createing note
+     const newNote = await noteModel.create({title, description});
+    
+        return res.status(201).json({
+            message: "Note Created successfully",
+            note: newNote,
+        })
+
+
+})
 
 export default app;
