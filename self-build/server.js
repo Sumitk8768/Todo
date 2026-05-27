1af4792dde1227f3c0b662b3ccc401d074251372
Sumit <sumitk72004@gmail.com>
2026-05-27 21:52:00 +0530

    Model created

--- self-build/server.js
+++ self-build/server.js
@@ -1,8 +1,11 @@
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-
+// calling db function
 connectDB()
+
+// server is running
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
+
